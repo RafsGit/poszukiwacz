d9c0bf8b637467e67e869f46fb03d5ff60561fbf
RafsGit <rafal.pater@hotmail.com>
2017-02-19 16:15:21 +0100

    Basic app structure

---
+++ src/App/MyApp.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rafal
+ * Date: 2/19/17
+ * Time: 4:11 PM
+ */
---
+++ web/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rafal
+ * Date: 2/19/17
+ * Time: 4:12 PM
+ */
+
+class MyApp {
+    public function hello(){
+        echo "Obecny!";
+    }
+}
